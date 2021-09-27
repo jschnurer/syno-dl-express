665ad9968e44bc1d2eb4093fa915b8bf3c715f00
Justin Schnurer <JSchnurer@slb.com>
2021-09-27 16:54:50 -0400

    first

---
+++ downloader.js
@@ -0,0 +1,182 @@
+const Syno = require('syno');
+
+async function handleUrlsAsync(urls, settings, outputProgressMessage) {
+  if (!urls.length) {
+    throw new Error("No urls were provided!");
+  }
+
+  var syno = new Syno({
+    protocol: settings.protocol,
+    host: settings.host,
+    port: settings.port,
+    account: settings.account,
+    passwd: settings.password,
+    apiVersion: settings.apiVersion || '6.0.2',
+  });
+
+  await createNestedFolders(urls, syno, settings, outputProgressMessage);
+  await createDownloadTasks(urls, syno, settings, outputProgressMessage);
+}
+
+async function createDownloadTasks(urls, syno, settings, outputProgressMessage) {
+  for (let i = 0; i < urls.length; i++) {
+    let destination = `${settings.baseDownloadDir}`
+
+    const folderPath = getFolderPath([urls[i]], settings)[0];
+
+    if (folderPath) {
+      destination += `/${folderPath}`;
+    }
+
+    if (destination.startsWith('/')) {
+      destination = destination.slice(1);
+    }
+
+    let url = tryInjectCredentials(urls[i], settings);
+
+    var promise = new Promise((resolve) => {
+      syno.dl.createTask({
+        uri: url,
+        destination: destination,
+      }, () => {
+        resolve();
+      });
+    });
+
+    await promise;
+
+    const msg = `Downloading ${urls[i]} to ${destination}`;
+    console.log(msg);
+
+    if (outputProgressMessage) {
+      outputProgressMessage(msg);
+    }
+  }
+}
+
+async function createNestedFolders(urls, syno, settings, outputProgressMessage) {
+  const folderPaths = getFolderPath(urls, settings);
+
+  for (let f = 0; f < folderPaths.length; f++) {
+    const path = folderPaths[f];
+    let folders = path.split('/')
+      .filter(x => !!x);
+
+    const folderName = folders.slice(-1);
+    parentFolders = folders.length > 1
+      ? folders.slice(0, folders.length)
+      : [];
+
+    if (parentFolders.length) {
+      // Create the path of folders leading to the folder name.
+      for (p = 0; p < parentFolders.length; p++) {
+        const parentPath = p === 0
+          ? settings.baseDownloadDir
+          : [settings.baseDownloadDir, ...parentFolders.slice(0, p)].join('/');
+
+        var promise = new Promise((resolve, reject) => {
+          syno.fs.createFolder({
+            folder_path: parentPath,
+            name: parentFolders[p],
+          }, (err) => {
+
+            if (err) {
+              reject(err);
+            }
+
+            resolve();
+          });
+        });
+
+        await promise;
+
+        const parentMessage = `Created ${[parentPath, parentFolders[p]].join('/')}`;
+        console.log(parentMessage);
+        if (outputProgressMessage) {
+          outputProgressMessage(parentMessage);
+        }
+      }
+
+    } else {
+      // Now create the folder itself.
+      let folder_path = parentFolders.length === 0
+        ? settings.baseDownloadDir
+        : [settings.baseDownloadDir, ...parentFolders].join('/');
+
+      var promise = new Promise(resolve => {
+        syno.fs.createFolder({
+          folder_path: folder_path,
+          name: folderName,
+        }, (err) => {
+
+          if (err) {
+            reject(err);
+          }
+
+          resolve();
+        });
+      });
+
+      await promise;
+
+      const createdMessage = `Created ${settings.baseDownloadDir}/${path}`;
+      console.log(createdMessage);
+      if (outputProgressMessage) {
+        outputProgressMessage(createdMessage);
+      }
+    }
+  }
+}
+
+function getFolderPath(urls, settings) {
+  const folderPaths = [];
+
+  urls.forEach(x => {
+    const domainHandler = findDomainHandler(x, settings);
+
+    if (!domainHandler) {
+      return;
+    }
+
+    let ix = x.indexOf(domainHandler.createPathAfterUrlComponent);
+    if (ix === -1) {
+      return;
+    }
+
+    ix += domainHandler.createPathAfterUrlComponent.length;
+
+    if (x.lastIndexOf('/') <= ix) {
+      return;
+    }
+
+    let path = decodeURI(x.substring(ix, x.lastIndexOf('/'))).split('/').map(x => !isNaN(Number(x))
+      ? `_${x}`
+      : x).join('/');
+
+    folderPaths.push(path);
+  });
+
+  return [...new Set(folderPaths)];
+}
+
+function findDomainHandler(url, settings) {
+  return settings.domainHandlers.find(h =>
+    url.toLowerCase().indexOf(h.urlMatch.toLowerCase()) > -1
+  );
+}
+
+function tryInjectCredentials(url, settings) {
+  const domainHandler = findDomainHandler(url, settings);
+
+  if (!domainHandler
+    || !domainHandler.username
+    || !domainHandler.password) {
+    return url;
+  }
+
+  return url.replace('://', `://${domainHandler.username}:${domainHandler.password}@`);
+}
+
+module.exports = {
+  handleUrlsAsync,
+};
---
+++ server.js
@@ -0,0 +1,52 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const downloader = require("./downloader.js");
+const settings = require("./local.settings.json");
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/submitUrls', async function (req, res) {
+  try {
+    await downloader.handleUrlsAsync(urls, settings.synoSettings);
+
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).send(err.toString());
+  }
+});
+
+io.on('connection', async function (socket) {
+  console.log('A user connected');
+
+  // Handler for submitting urls.
+  socket.on('submitUrls', async function (urls) {
+    console.log(`Received urls:\n${JSON.stringify(urls, null, 2)}`);
+
+    io.emit("started");
+
+    try {
+      await downloader.handleUrlsAsync(urls,
+        settings.synoSettings,
+        (progressMessage) => {
+          io.emit('progress', progressMessage);
+        });
+
+      console.log("Done.");
+      io.emit("done");
+    } catch (err) {
+      console.log(err);
+      io.emit('error', err.toString());
+    }
+  });
+
+  socket.on('disconnect', function () {
+    console.log('A user disconnected');
+  });
+});
+
+http.listen(settings.port, function () {
+  console.log(`listening on *:${settings.port}`);
+});
